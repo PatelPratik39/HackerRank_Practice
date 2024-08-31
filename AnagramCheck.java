e6392b29a9d4cf6383a60da237e4fd5870d6504a
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-31 16:40:01 -0500

    Anagrams Check

---
+++ AnagramCheck.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+/**
+ * Two strings,  and , are called anagrams if they contain all the same characters in the same frequencies. For this challenge, the test is not case-sensitive. For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
+ *
+ * Function Description
+ *
+ * Complete the isAnagram function in the editor.
+ *
+ * isAnagram has the following parameters:
+ *
+ * string a: the first string
+ * string b: the second string
+ * Returns
+ *
+ * boolean: If  and  are case-insensitive anagrams, return true. Otherwise, return false.
+ * Input Format
+ *
+ * The first line contains a string .
+ * The second line contains a string .
+ *
+ * Constraints
+ *
+ * Strings  and  consist of English alphabetic characters.
+ * The comparison should NOT be case sensitive.
+ * Sample Input 0
+ *
+ * anagram
+ * margana
+ * Sample Output 0
+ *
+ * Anagrams
+ */
+
+public class AnagramCheck {
+
+    private static boolean isAnagram ( String str1, String str2 ) {
+
+//        base case
+        if(str1.length() != str2.length()){
+            return false;
+        }
+//        frequency check of the strings
+        int[] frequency = new int[26];
+
+        for(int i = 0; i < str1.length(); i++){
+            frequency[str1.charAt(i) - 'a']++;
+            frequency[str2.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(frequency[i] != 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void main ( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine().trim().toLowerCase();
+        String str2 = scanner.nextLine().trim().toLowerCase();
+
+//        check Anangrams
+        if(isAnagram(str1, str2)){
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
+        }
+
+    }
+
+
+}
